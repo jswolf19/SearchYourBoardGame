0f70066b5b44b36b0bb76bd805707c2899cbe977
ContinueFine <continue.fine@gmail.com>
2020-08-23 19:45:24 +0900

    構文エラー修正

--- getCsvData.js
+++ getCsvData.js
@@ -7,8 +7,8 @@ function getCsvData(dataUrl) {
         if(request.readyState == 4 && request.status == 200){
             //outputElement.innerHTML = request.responseText
             document.getElementById("display").innerHTML = request.responseText;
-        };
-    }
+        }
+    };
     request.open('GET', dataUrl, true);
     request.send(null);    
 /*
@@ -18,6 +18,7 @@ function getCsvData(dataUrl) {
     });
     request.open('GET', dataUrl, true);
     request.send(null);
-}
 */
+}
+
 getCsvData('https://docs.google.com/spreadsheets/d/e/2PACX-1vT1PU7m8bNHsSdLkTssjuG8l8s0wUGSGsEXHqP5kuylHZM7O2eKSJcd8Lu1q2T9JzEzTBHuvjeUgAlt/pub?gid=747156868&single=true&output=csv')

3fbc1b275b083e2680d44afb834c5e433e0cd6bd
ContinueFine <continue.fine@gmail.com>
2020-08-23 20:24:52 +0900

    実行確認用テストコミット

--- getCsvData.js
+++ getCsvData.js
@@ -2,7 +2,7 @@ const outputElement = document.getElementById('output_csv');
 
 function getCsvData(dataUrl) {
     const request = new XMLHttpRequest();
-
+/*
     request.onreadystatechange = function(){
         if(request.readyState == 4 && request.status == 200){
             //outputElement.innerHTML = request.responseText
@@ -10,15 +10,16 @@ function getCsvData(dataUrl) {
         }
     };
     request.open('GET', dataUrl, true);
-    request.send(null);    
-/*
+    request.send(null);
+*/
+
     request.addEventListener('load', (event) => {
         const response = event.target.responseText;
         outputElement.innerHTML = response;
     });
     request.open('GET', dataUrl, true);
     request.send(null);
-*/
+
 }
 
 getCsvData('https://docs.google.com/spreadsheets/d/e/2PACX-1vT1PU7m8bNHsSdLkTssjuG8l8s0wUGSGsEXHqP5kuylHZM7O2eKSJcd8Lu1q2T9JzEzTBHuvjeUgAlt/pub?gid=747156868&single=true&output=csv')

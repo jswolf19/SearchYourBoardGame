03963412aa3aa42f99af1c7f30f954ed91b76388
ContinueFine <continue.fine@gmail.com>
2020-08-24 21:24:13 +0900

    配列データを表としてHTML表示するように変更

--- getSpreadData.js
+++ getSpreadData.js
@@ -1,41 +1,41 @@
-const outputElement = document.getElementById('output_spread');
-
 function getSpreadData(){
+    const MAX_ROW = 10000;
+    const START_ROW = 0;
     const store = new SteinStore("https://api.steinhq.com/v1/storages/5f43a0655d3cdc44fcd7d382");
-      store.read("test", { limit: 100, offset: 0 }).then(data => {
+      store.read("test", {limit: MAX_ROW, offset: START_ROW}).then(data => {
+          //デバッグ用データ内容表示↓↓↓↓↓↓↓↓↓↓
           console.dir(data);
           console.log(" - - - - - - - - - - - ");
-          for (let i = 0; i < data.length; i++) {
+          for(let i = 0; i < data.length; i++){
                 const element = data[i];
-                console.log("title:"+element.text);
+                console.log("name:"+element.text);
           }
+          //デバッグ用データ内容表示↑↑↑↑↑↑↑↑↑↑
       });
+      return data
 }
 
-getSpreadData()
-
-/*
-function getCsvData(dataUrl) {
-    const request = new XMLHttpRequest();
-
-    request.onreadystatechange = function(){
-        if(request.readyState == 4 && request.status == 200){
-            //outputElement.innerHTML = request.responseText
-            document.getElementById("display").innerHTML = request.responseText;
+function makeTable(data, tableId){
+    var rows=[];
+    var table = document.createElement("table");
+    for(rowNo = 0; rowNo < data.length; rowNo++){
+        rows.push(table.insertRow(-1)); //行追加
+        for(columnNo = 0; columnNo < data[0].length; columnNo++){
+            cell = rows[rowNo].insertCell(-1); //列追加
+            cell.appendChild(document.createTextNode(data[rowNo][columnNo])); //データ追加
+            //背景色の設定
+            if(rowNo == 0){
+                cell.style.backgroundColor = "#bbb"; //ヘッダ行
+            }else{
+                cell.style.backgroundColor = "#ddd"; //データ行
+            }
         }
-    };
-    request.open('GET', dataUrl, true);
-    request.send(null);
-
-
-    request.addEventListener('load', (event) => {
-        const response = event.target.responseText;
-        outputElement.innerHTML = response;
-    });
-    request.open('GET', dataUrl, true);
-    request.send(null);
-
+    }
+    //指定したdiv要素に表を加える
+    document.getElementById(tableId).appendChild(table);
 }
 
-getCsvData('https://script.google.com/macros/s/AKfycbx0zpemQ7kHliQX_NLItfD4PhoykuzbOcMWncYfSK0O04GKXv4/exec')
-*/
+window.onload = function(){
+    var data = getSpreadData()
+    makeTable(data, "table")
+};

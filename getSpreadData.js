4ac09ec2a6975fd53ff6833c26676fa222a305e8
ContinueFine <continue.fine@gmail.com>
2020-08-24 23:29:30 +0900

    表が正しく表示されるように修正

--- getSpreadData.js
+++ getSpreadData.js
@@ -2,40 +2,44 @@ function getSpreadData(){
     const MAX_ROW = 10000;
     const START_ROW = 0;
     const store = new SteinStore("https://api.steinhq.com/v1/storages/5f43a0655d3cdc44fcd7d382");
-      store.read("test", {limit: MAX_ROW, offset: START_ROW}).then(data => {
-          //デバッグ用データ内容表示↓↓↓↓↓↓↓↓↓↓
-          console.dir(data);
-          console.log(" - - - - - - - - - - - ");
-          for(let i = 0; i < data.length; i++){
-                const element = data[i];
-                console.log("name:"+element.text);
-          }
-          //デバッグ用データ内容表示↑↑↑↑↑↑↑↑↑↑
-      });
-      return data
+    store.read("test", {limit: MAX_ROW, offset: START_ROW}).then(data => {
+        //デバッグ用データ内容表示↓↓↓↓↓↓↓↓↓↓
+        console.log(" - - - - - - - - - - - ");
+        console.dir(data);
+        console.log(" - - - - - - - - - - - ");
+        //デバッグ用データ内容表示↑↑↑↑↑↑↑↑↑↑
+        makeTable(data, "table")
+    });
 }
 
 function makeTable(data, tableId){
     var rows=[];
     var table = document.createElement("table");
+
+    //ヘッダ行
+    var keys = Object.keys(data[0]);
+    rows.push(table.insertRow(-1)); //行追加
+    keys.forEach(element => {
+        cell = rows[0].insertCell(-1); //列追加
+        cell.appendChild(document.createTextNode(element)); //データ追加
+        cell.style.backgroundColor = "#bbb"; //背景色の設定
+    });
+
+    //データ行
     for(rowNo = 0; rowNo < data.length; rowNo++){
         rows.push(table.insertRow(-1)); //行追加
-        for(columnNo = 0; columnNo < data[0].length; columnNo++){
-            cell = rows[rowNo].insertCell(-1); //列追加
-            cell.appendChild(document.createTextNode(data[rowNo][columnNo])); //データ追加
-            //背景色の設定
-            if(rowNo == 0){
-                cell.style.backgroundColor = "#bbb"; //ヘッダ行
-            }else{
-                cell.style.backgroundColor = "#ddd"; //データ行
-            }
-        }
+        var row = data[rowNo]
+        keys.forEach(element => {
+            cell = rows[rowNo+1].insertCell(-1); //列追加
+            cell.appendChild(document.createTextNode(row[element])); //データ追加
+            cell.style.backgroundColor = "#ddd"; //背景色の設定
+        });
     }
+
     //指定したdiv要素に表を加える
     document.getElementById(tableId).appendChild(table);
 }
 
 window.onload = function(){
-    var data = getSpreadData()
-    makeTable(data, "table")
+    getSpreadData()
 };
